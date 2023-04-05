5dccaa01246c2954439fbdb16d74dd8a46711b20
исп-34 <-34@LAB25-03>
2023-04-05 10:48:57 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace КалькуляторВарламов
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Height += 25;
+        }
+
+        private void Calculate(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Plus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Minus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Proizv(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
